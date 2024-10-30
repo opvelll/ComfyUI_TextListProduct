f3bf63e1fbdd410bf49bea016dc9f917424c35a4
opvelll <opvel.hero@gmail.com>
2024-10-31 00:52:49 +0900

    feat: Add PromptPairConcat class to node mappings

--- __init__.py
+++ __init__.py
@@ -4,12 +4,15 @@ from .src.TextListProductWithSingleB import TextListProductWithSingleB
 from .src.TextListProductWithSingleBoth import TextListProductWithSingleBoth
 from .src.ProductedString import ProductedString
 
+from .src.PromptPairConcat import PromptPairConcat
+
 NODE_CLASS_MAPPINGS = {
     "TextListProduct": TextListProduct,
     "TextListProductWithSingleA": TextListProductWithSingleA,
     "TextListProductWithSingleB": TextListProductWithSingleB,
     "TextListProductWithSingleBoth": TextListProductWithSingleBoth,
     "ProductedString": ProductedString,
+    "PromptPairConcat": PromptPairConcat,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -18,4 +21,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "TextListProductWithSingleB": "Text List Product With Single B",
     "TextListProductWithSingleBoth": "Text List Product With Single Both",
     "ProductedString": "Producted String",
+    "PromptPairConcat": "Prompt Pair Concat",
 }
